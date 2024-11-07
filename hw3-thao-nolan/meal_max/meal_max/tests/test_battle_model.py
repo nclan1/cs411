c40457bb16fd7724dbabd21c277ed5fd345a347e
Thao Dang Hoang <thaodh03@gmail.com>
2024-11-07 14:55:40 -0500

    added test_clear unit test

--- hw3-thao-nolan/meal_max/meal_max/tests/test_battle_model.py
+++ hw3-thao-nolan/meal_max/meal_max/tests/test_battle_model.py
@@ -5,19 +5,37 @@ import pytest
 from meal_max.models.kitchen_model import Meal, update_meal_stats
 from meal_max.utils.logger import configure_logger
 from meal_max.utils.random_utils import get_random
+from meal_max.models.battle_model import BattleModel
+
+@pytest.fixture
+def sample_combatant_1():
+    return Meal(1, "testName", "testCuisine", 0.5, "LOW")
 
 
-def test_battle(): {
-"""Test battle"""
-}
 
-def test_valid_num_combatants(mock_cursor): {
+def test_battle(): 
+    """Test battle"""
 
-}
 
-def test_clear_combatants(): {
-"""check to see if clear the combatants array"""
-}
+
+def test_valid_num_combatants(mock_cursor): 
+    """Testing for the valid number of combatants"""
+
+
+
+def test_clear_combatants(): 
+    """check to see if clear the combatants array"""
+    sampleBattle = BattleModel()
+    sampleBattle.combatants[0] = sample_combatant_1
+
+    # check that meal has been added to the array
+    assert len(sampleBattle.combatants) == 1 
+
+    sampleBattle.clear()
+    # check that meal has been cleared
+    assert len(sampleBattle.combatants) == 0
+    
+
 
 def test_empty_clear_combatants():{
 
