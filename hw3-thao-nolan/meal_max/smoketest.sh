99e5d497a811c1adfc83ffadb9f702b74be37319
nolan <144292887+nclan1@users.noreply.github.com>
2024-11-11 10:43:41 -0500

    added 5 more smoketests from the bottom

--- hw3-thao-nolan/meal_max/smoketest.sh
+++ hw3-thao-nolan/meal_max/smoketest.sh
@@ -37,7 +37,127 @@ check_db() {
   else
     echo "Database check failed."
     exit 1
+}
+
+
+
+
+
+
+
+# Function to initiate battle between combatants
+
+battle() {
+  echo "Initiating Battle..."
+  response=$(curl -s -X GET "$BASE_URL/battle")
+
+  if echo "$response" | grep -q '"status": "battle complete"'; then
+    echo "Battle complete"
+    if [ "$ECHO_JSON" = true ]; then
+      echo "Current Battle JSON:"
+      echo "$response" | jq . 
+    fi
+  else
+    echo "Failed to get battle started."
+    exit 1
   fi
 }
 
+
+
+# Function to clear combatants
+
+clear_combatants() {
+  echo "Clearing combatants..."
+  response=$(curl -s -X POST "$BASE_URL/clear-combatants")
+
+  if echo "$response" | grep -q '"status": "combatants cleared"'; then
+    echo "Combatants cleared successfully."
+  else
+    echo "Failed to clear playlist."
+    exit 1
+  fi
+}
+
+
+# Function to get combatants
+
+get_combatants() {
+  echo "Getting current combatants..."
+  response=$(curl -s -X GET "$BASE_URL/get-combatants")
+
+  if echo "$response" | grep -q '"status": "success"'; then
+    echo "Current combatant retrieved successfully."
+    if [ "$ECHO_JSON" = true ]; then
+      echo "Current Combatant JSON:"
+      echo "$response" | jq . 
+    fi
+  else
+    echo "Failed to retrieve current combatant."
+    exit 1
+  fi
+}
+
+
+# Function to prep a meal to become a combatant
+
+prep_combatant() {
+  meal_name=$1
+
+  echo "Preparing combatant ($meal_name) for battle.."
+  response=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"meal\": \"$meal_name\"}" "$BASE_URL/api/prep-combatant")
+  if echo "$response" | grep -q '"status": "combatant prepared"'; then
+    echo "Successfuly prepared combatant ($meal_name)."
+  else
+    echo "Failed to prepare combatant ($meal_name)."
+    exit 1
+  fi
+}
+
+
+
+# Function to get the meal leaderboard sorted
+
+get_leader_board_by_wins() {
+  echo "Getting leaderboard of meals sorted by wins..."
+  response=$(curl -s -X GET "$BASE_URL/leaderboard?sort=wins")
+  if echo "$response" | grep -q '"status": "success"'; then
+    echo "Leaderboard retrieved successfully."
+    if [ "$ECHO_JSON" = true ]; then
+      echo "Meal Leaderboard JSON (sorted by wins):"
+      echo "$response" | jq .
+    fi
+  else
+    echo "Failed to get meal leaderboard."
+    exit 1
+  fi
+}
+get_leader_board_by_battles() {
+  echo "Getting leaderboard of meals sorted by wins..."
+  response=$(curl -s -X GET "$BASE_URL/leaderboard?sort=battles")
+  if echo "$response" | grep -q '"status": "success"'; then
+    echo "Leaderboard retrieved successfully."
+    if [ "$ECHO_JSON" = true ]; then
+      echo "Meal Leaderboard JSON (sorted by wins):"
+      echo "$response" | jq .
+    fi
+  else
+    echo "Failed to get meal leaderboard."
+    exit 1
+  fi
+}
+get_leader_board_by_win_pct() {
+  echo "Getting leaderboard of meals sorted by wins..."
+  response=$(curl -s -X GET "$BASE_URL/leaderboard?sort=win_pct")
+  if echo "$response" | grep -q '"status": "success"'; then
+    echo "Leaderboard retrieved successfully."
+    if [ "$ECHO_JSON" = true ]; then
+      echo "Meal Leaderboard JSON (sorted by wins):"
+      echo "$response" | jq .
+    fi
+  else
+    echo "Failed to get meal leaderboard."
+    exit 1
+  fi
+}
 
